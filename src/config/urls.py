7fc6050900bb35dec004399c523f8ca561c93a83
pr00fesseur <qq7337047@gmail.com>
2023-10-22 05:18:41 +0200

    framework beggining

--- src/config/urls.py
+++ src/config/urls.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+# # controller
+# def foo(request):
+#     data = get_message()  # model
+#     response = JsonResponse(data)  # JsonResponse is view
+#     # controller
+#     return response
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("users/", include("users.urls")),
+    path("auth/", include("authentication.urls"))
 ]
